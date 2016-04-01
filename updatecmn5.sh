15f5d20ffa492fb67105a6fdbf47234c6ff5a228
Taylor Kline <taylor.kline@streamingfreebies.com>
2016-04-01 16:07:49 -0500

    Updated script to update CM13 for hammerhead

--- updatecmn5.sh
+++ updatecmn5.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
-#script created by Taylor Kline
-#Taylor.Kline@streamingfreebies.com
-cmn5directory=/media/development/n5cm11
-rootdirectory=/media/development
+# script created by Taylor Kline
+# Taylor.Kline@streamingfreebies.com
+cmn5directory=~/Desktop/android/cm13
 echo 'Updating the Nexus 5 CM11 repository located at:' $cmn5directory
 cd $cmn5directory
-echo 'Beginning by updating the repo command.'
-repo selfupdate
-echo 'Repo command updated.'
-repo sync
+echo 'Beginning by syncing the directory.'
+# repo sync
 echo 'Nexus 5 CM11 repository updated!'
 echo "Updating breakfast for hammerhead."
 source build/envsetup.sh
 breakfast hammerhead
 echo "Updating proprietary blobs for hammerhead."
-cd ../lge_proprietary/
+cd $cmn5directory/../proprietary_vendor_lge/
 git pull
-cp -r ./hammerhead/proprietary $rootdirectory/system
-cd $cmn5directory/device/lge/hammerhead/
+cp -r ./hammerhead/proprietary $cmn5directory/../system
 echo "Adding the updated blobs to repo."
-./extract-files.sh $rootdirectory
-croot
-rm -rf $rootdirectory/system
-rm $cmn5directory/vendor/lge/hammerhead/proprietary/Android.mk
+cd $cmn5directory/device/lge/hammerhead
+./extract-files.sh $cmn5directory/../system
+rm -rf $cmn5directory/../system
 echo "Update completed, visit" $cmn5directory "for your updated files."
 echo "Don't forget to execute 'source build/envsetup.sh' to be able to run brunch hammerhead."
